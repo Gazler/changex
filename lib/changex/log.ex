129d675c7b9bf993bd26ae07088a9822f34d7bc3
Gary Rennie <webmaster@gazler.com>
2014-08-18 21:59:23 +0100

    docs(Changex.Log): add documentation for log method
    Previously the `log` function was undocumented. The documentation has
    been added, however due to the tests depending on getting the commit
    hashes from git, a doctest has not been added.

--- lib/changex/log.ex
+++ lib/changex/log.ex
@@ -1,4 +1,21 @@
 defmodule Changex.Log do
+  @moduledoc """
+  This module will get a list of commits based on the git log on a git
+  repository.
+  """
+
+  @doc """
+  Output a log in the format:
+
+      [
+        ["f329668f8661795f6e2b3c9fd7b7ae6de7dc3789", "fix(baz): set baz"],
+        ["6a02160216344cf84f8ec7b13bb10a29f2caa340", "chore(bar): set bar"],
+      ]
+
+  An optional `dir` argument can be passed in, if present then this
+  location will be used for the git repository instead of the current
+  directory.
+  """
   def log(dir \\ nil) do
     args = ["log", "--pretty=format:%H%n%s%n%b==END==", "--no-merges"]
     if dir != nil do
